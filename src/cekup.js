807d818977a0170f422b7ed968387637280f33df
luiz fernando <luizmariano286@gmail.com>
2021-03-13 12:02:51 -0300

    First commit

---
+++ src/cekup.js
@@ -0,0 +1,23 @@
+const cekup = (prefix) => { 
+
+	return `
+	
+「 *UPDATING* 」
+
+SEMUANYA FITUR TERBARU
+MENEMUKAN BUG?
+KETIK *${prefix}laporbug*
+REQWEST FITUR?
+KETIK *${prefix}reqwest*
+
+
+*NOTE :*
+
+[❗]  *SUBSCRIBE CHANNEL MR KING*
+
+ 
+`
+
+}
+
+exports.cekup = cekup
